5210994ebf006744865a1549ca9a27e2e6eb0bbd
Sofi <sofia_pereyra95@hotmail.com>
2022-05-15 19:48:35 -0300

    Subo logica de encriptar y desencriptar

---
+++ encriptador.js
@@ -0,0 +1,62 @@
+var botonEncriptar = document.querySelector("#button-encriptar");
+var botonDesencriptar = document.querySelector("#button-desencriptar");
+
+function pintarResultado(dato) {
+  var resultadoPlaceholder = document.querySelector("#resultado-placeholder");
+  var resultadoContainer = document.querySelector("#resultado-container");
+  var resultado = document.querySelector("#resultado");
+
+  resultadoPlaceholder.classList.add("hidden");
+  resultadoContainer.classList.remove("hidden");
+  resultado.textContent = dato;
+}
+
+function encriptar() {
+  var textoEntrada = document.querySelector("#text-input").value;
+
+  var listaDeLetras = textoEntrada.split("");
+  var listaEncriptada = listaDeLetras.map(function (letra) {
+    if (letra === "e") {
+      return "enter";
+    }
+    if (letra === "i") {
+      return "imes";
+    }
+    if (letra === "a") {
+      return "ai";
+    }
+    if (letra === "o") {
+      return "ober";
+    }
+    if (letra === "u") {
+      return "ufat";
+    }
+    return letra;
+  });
+  var resultado = listaEncriptada.join("");
+
+  pintarResultado(resultado);
+}
+
+function desencriptar() {
+  // obtener el texto
+  var textoEntrada = document.querySelector("#text-input").value;
+  // encriptar el texto
+  var resultado = textoEntrada
+    .split("enter")
+    .join("e")
+    .split("imes")
+    .join("i")
+    .split("ai")
+    .join("a")
+    .split("ober")
+    .join("o")
+    .split("ufat")
+    .join("u");
+
+  // pintar el texto encriptado
+  pintarResultado(resultado);
+}
+
+botonEncriptar.addEventListener("click", encriptar);
+botonDesencriptar.addEventListener("click", desencriptar);
